f558a350c2c5424ba13f42bc398dc0f756a9c4f1
Charan Williams <charanwilliams@gmail.com>
2023-07-31 20:34:43 -0500

    Overhauled dashboard page layout. Made Query page centered.

--- pages/📊 Dashboard.py
+++ pages/📊 Dashboard.py
@@ -23,60 +23,68 @@ def initialize():
     confusion_matrix = joblib.load("diabetes_confusion_matrix.save")
     return confusion_matrix
 
-confusion_matrix= initialize()
-
-# Create accuracy table
-
-false_negative = confusion_matrix[1][0]
-true_negative = confusion_matrix[0][0]
-true_positive = confusion_matrix[1][1]
-false_positive = confusion_matrix[0][1]
-
-confusion_table = go.Figure(
-    data = [go.Bar(
-        x = ["False Negative", "True Negative" , "True Positive", "False Positive"],
-        y = [false_negative, true_negative, true_positive, false_positive])], )
-
 # Save the data as a state variable so that it can be accessed by other pages
 if 'df' not in st.session_state:
     st.session_state.df = load_data()
 
 df_src = st.session_state.df
 
-st.title("📊 Data Visualization")
+st.title("📊 Dashboard")
 st.divider()
 
 st.header("Model Performance")
-st.divider()
+
+# Create accuracy table
+confusion_matrix= initialize()
+confusion_table = go.Figure(
+    data = [go.Bar(
+        x = ["False Negative", "True Negative" , "True Positive", "False Positive"],
+        y = [confusion_matrix[1][0], confusion_matrix[0][0], confusion_matrix[1][1], confusion_matrix[0][1]])])
+
+#colors={"False Negative": 'red', "True Negative": 'blue', "True Positive":'blue',"False Positive":'red'}
+
+confusion_table.update_layout()
+
+st.plotly_chart(confusion_table, use_container_width=True, height=300)
+
+
 scores = joblib.load("training_and_testing_scores.save")
 training_score = scores[0]
 testing_score = scores[1]
 st.subheader("Training Set Score = {}%".format(round(training_score, 3)*100))
 st.subheader("Testing Set Score = {}%".format(round(testing_score, 3)*100))
-
-st.plotly_chart(confusion_table, use_container_width=True, height=500)
+st.divider()
 
 # Create 2 column layout
+st.header('Gender Breakdown')
 col1, col2 = st.columns(2, gap='large')
+with col1:
+    # Create gender breakdown dataframe and pie chart
+    diabetes_negative_gender_totals = df_src.query('diabetes == 0')['gender'].value_counts().reset_index()
+    st.subheader('Diabetes Negative')
+    st.plotly_chart(px.pie(data_frame=diabetes_negative_gender_totals, names="index", values='gender', color='index', color_discrete_map={'Male': '#57799E', 'Female': '#DAA49A', 'Other': '#41818B'}, hover_name='index', hover_data='gender'), use_container_width=True, height=200)
 
-# Create gender breakdown dataframe and pie chart
-diabetes_negative_gender_totals = df_src.query('diabetes == 0')['gender'].value_counts().reset_index()
-col1.plotly_chart(px.pie(data_frame=diabetes_negative_gender_totals, names="index", values='gender', title='Diabetes Negative Gender Breakdown', color='index', color_discrete_map={'Male': '#57799E', 'Female': '#DAA49A', 'Other': '#41818B'}, hover_name='index', hover_data='gender', width=750, height=750), use_container_width=True, height=500)
+with col2:
+    diabetes_positive_gender_totals = df_src.query('diabetes == 1')['gender'].value_counts().reset_index()
+    st.subheader('Diabetes Positive')
+    st.plotly_chart(px.pie(data_frame=diabetes_positive_gender_totals, names="index", values='gender', color='index', color_discrete_map={'Male': '#57799E', 'Female': '#DAA49A', 'Other': '#41818B'}, hover_name='index', hover_data='gender'), use_container_width=True, height=200)
 
-
-diabetes_positive_gender_totals = df_src.query('diabetes == 1')['gender'].value_counts().reset_index()
-col2.plotly_chart(px.pie(data_frame=diabetes_positive_gender_totals, names="index", values='gender', title='Diabetes Positive Gender Breakdown', color='index', color_discrete_map={'Male': '#57799E', 'Female': '#DAA49A', 'Other': '#41818B'}, hover_name='index', hover_data='gender', width=750, height=750), use_container_width=True, height=500)
-
+st.divider()
+st.header("BMI, A1C, and Age Distribution")
 # Create dataframe with all bmi data and only bmi data of those who have diabetes
-diabetes_negative_bmi_data = go.Histogram(x=df_src.query('diabetes == 0')['bmi'], marker=dict(color='#6E8EAF'), nbinsx=20, name="Diabetes Negative")
-diabetes_positive_bmi_data = go.Histogram(x=df_src.query('diabetes == 1')['bmi'], marker=dict(color='#F52D00', opacity= .4), nbinsx=20, name="Diabetes Positive")
-
+diabetes_negative_bmi_data = go.Histogram(x=df_src.query('diabetes == 0')['bmi'], marker=dict(color='#6E8EAF'),
+                                          nbinsx=20, name="Diabetes Negative")
+diabetes_positive_bmi_data = go.Histogram(x=df_src.query('diabetes == 1')['bmi'],
+                                          marker=dict(color='#F52D00', opacity=.4), nbinsx=20,
+                                          name="Diabetes Positive")
 # Create histogram to plot bmi distribution
 bmi_hist = make_subplots(specs=[[{"secondary_y": True}]])
 bmi_hist.add_trace(diabetes_negative_bmi_data, secondary_y=False)
 bmi_hist.add_trace(diabetes_positive_bmi_data, secondary_y=True)
-bmi_hist.update_layout(title='BMI Distribution', xaxis_title='BMI', yaxis_title='Count', legend_title='Key', width=1200, height=700)
-col1.plotly_chart(bmi_hist, use_container_width=True, height=300)
+bmi_hist.update_layout(xaxis_title='BMI', yaxis_title='Count', legend_title='Key')
+
+st.subheader('BMI Distribution')
+st.plotly_chart(bmi_hist, use_container_width=True, height=300)
 
 # Create dataframe with all A1C data and only A1C data of those who have diabetes
 diabetes_negative_a1c_data = go.Histogram(x=df_src.query('diabetes == 0')['HbA1c_level'], marker=dict(color='#6E8EAF'), nbinsx=10, name="Diabetes Negative")
@@ -86,75 +94,95 @@ diabetes_positive_a1c_data= go.Histogram(x=df_src.query('diabetes == 1')['HbA1c_
 a1c_hist = make_subplots(specs=[[{"secondary_y": True}]])
 a1c_hist.add_trace(diabetes_negative_a1c_data, secondary_y=False)
 a1c_hist.add_trace(diabetes_positive_a1c_data, secondary_y=True)
-a1c_hist.update_layout(title='A1C Distribution', xaxis_title='A1C', yaxis_title='Count', legend_title='Key', width=1200, height=700)
+a1c_hist.update_layout(xaxis_title='A1C', yaxis_title='Count', legend_title='Key')
 
-
-col2.plotly_chart(a1c_hist, use_container_width=True, height=300)
+st.subheader('A1C Distribution')
+st.plotly_chart(a1c_hist, use_container_width=True, height=300)
 
 # Create dataframe with all age data and only age data of those who have diabetes
 diabetes_negative_age_data = go.Histogram(x=df_src.query('diabetes == 0')['age'], marker=dict(color='#6E8EAF'), nbinsx=100, name="Diabetes Negative")
 diabetes_positive_age_data= go.Histogram(x=df_src.query('diabetes == 1')['age'], marker=dict(color='#F52D00', opacity= .4), nbinsx=100, name="Diabetes Positive")
 
-# Create histogram to plot A1C distribution
+# Create histogram to plot age distribution
 age_hist = make_subplots(specs=[[{"secondary_y": True}]])
 age_hist.add_trace(diabetes_negative_age_data, secondary_y=False)
 age_hist.add_trace(diabetes_positive_age_data, secondary_y=True)
-age_hist.update_layout(title='Age Distribution', xaxis_title='Age', yaxis_title='Count', legend_title='Key', width=1200, height=700)
-st.plotly_chart(age_hist, use_container_width=True, height=500)
+age_hist.update_layout(xaxis_title='Age', yaxis_title='Count', legend_title='Key')
 
-"Let's see what the first few rows look like:"
-st.write(df_src.head(5))
-"Let's see what's filled out:"
-(df_src.count()/len(df_src))*100
-"Let's get more information. Let's see the counts of each value:"
-for i in range(0, len(df_src.columns)):
-    st.write(df_src.columns[i])
-    st.write(df_src.iloc[:,i].value_counts())
+st.subheader('Age Distribution')
+st.plotly_chart(age_hist, use_container_width=True, height=300)
 
-"Lets see what percentage of those who have diabetes are male:"
+st.divider()
+st.header('Data Analysis')
 
-st.write(len(df_src.query("diabetes == 1 and gender=='Male'"))/len(df_src.query('diabetes==1'))*100)
+col3, col4 = st.columns(2, gap='small')
+with col3:
+    st.subheader('Data Completion')
+    st.table((df_src.count()/len(df_src))*100)
 
-"Lets see what percentage of those who have diabetes are female:"
+with col4:
+    st.subheader('Counts')
+    st.table(df_src['hypertension'].value_counts())
+    st.table(df_src['heart_disease'].value_counts())
+    st.table(df_src['diabetes'].value_counts())
+    st.table(df_src['smoking_history'].value_counts())
 
-st.write(len(df_src.query("diabetes == 1 and gender == 'Female'"))/len(df_src.query('diabetes==1'))*100)
 
-"Lets see the average age for those who don't have diabetes:"
+negative_stats_table = pd.DataFrame(index= ['Diabetes Negative',], columns=['Mean', 'Median', 'Mode', 'Min', 'Max'])
+positive_stats_table = pd.DataFrame(index= ['Diabetes Positive',], columns=['Mean', 'Median', 'Mode', 'Min', 'Max'])
 
-st.write(df_src.query('diabetes == 0')['age'].mean())
+st.header('Diabetes Negative Stats')
+negative_stats_table = pd.DataFrame(data={'Mean':[df_src.query('diabetes == 0')['age'].mean(),
+                                                  df_src.query('diabetes == 0')['bmi'].mean(),
+                                                  df_src.query('diabetes== 0')['HbA1c_level'].mean(),
+                                                  df_src.query('diabetes== 0')['blood_glucose_level'].mean()
+                                                  ],
+                                          'Median':[df_src.query('diabetes == 0')['age'].median(),
+                                                  df_src.query('diabetes == 0')['bmi'].median(),
+                                                  df_src.query('diabetes== 0')['HbA1c_level'].median(),
+                                                  df_src.query('diabetes== 0')['blood_glucose_level'].median()],
+                                          'Mode':[df_src.query('diabetes == 0')['age'].mode()[0],
+                                                  df_src.query('diabetes == 0')['bmi'].mode()[0],
+                                                  df_src.query('diabetes== 0')['HbA1c_level'].mode()[0],
+                                                  df_src.query('diabetes== 0')['blood_glucose_level'].mode()[0]],
+                                          'Min':[df_src.query('diabetes == 0')['age'].min(),
+                                                  df_src.query('diabetes == 0')['bmi'].min(),
+                                                  df_src.query('diabetes== 0')['HbA1c_level'].min(),
+                                                  df_src.query('diabetes== 0')['blood_glucose_level'].min()],
+                                          'Max':[df_src.query('diabetes == 0')['age'].max(),
+                                                  df_src.query('diabetes == 0')['bmi'].max(),
+                                                  df_src.query('diabetes== 0')['HbA1c_level'].max(),
+                                                  df_src.query('diabetes== 0')['blood_glucose_level'].max()],}, index=['Age', 'BMI', 'A1C', 'Blood Glucose'])
+st.table(negative_stats_table)
 
-"Lets see the average age for those who do have diabetes:"
+st.header('Diabetes Positive Stats')
+positive_stats_table = pd.DataFrame(data={'Mean':[df_src.query('diabetes == 1')['age'].mean(),
+                                                  df_src.query('diabetes == 1')['bmi'].mean(),
+                                                  df_src.query('diabetes== 1')['HbA1c_level'].mean(),
+                                                  df_src.query('diabetes== 1')['blood_glucose_level'].mean()
+                                                  ],
+                                          'Median':[df_src.query('diabetes == 1')['age'].median(),
+                                                  df_src.query('diabetes == 1')['bmi'].median(),
+                                                  df_src.query('diabetes== 1')['HbA1c_level'].median(),
+                                                  df_src.query('diabetes== 1')['blood_glucose_level'].median()],
+                                          'Mode':[df_src.query('diabetes == 1')['age'].mode()[0],
+                                                  df_src.query('diabetes == 1')['bmi'].mode()[0],
+                                                  df_src.query('diabetes== 1')['HbA1c_level'].mode()[0],
+                                                  df_src.query('diabetes== 1')['blood_glucose_level'].mode()[0]],
+                                          'Min':[df_src.query('diabetes == 1')['age'].min(),
+                                                  df_src.query('diabetes == 1')['bmi'].min(),
+                                                  df_src.query('diabetes== 1')['HbA1c_level'].min(),
+                                                  df_src.query('diabetes== 1')['blood_glucose_level'].min()],
+                                          'Max':[df_src.query('diabetes == 1')['age'].max(),
+                                                  df_src.query('diabetes == 1')['bmi'].max(),
+                                                  df_src.query('diabetes== 1')['HbA1c_level'].max(),
+                                                  df_src.query('diabetes== 1')['blood_glucose_level'].max()],}, index=['Age', 'BMI', 'A1C', 'Blood Glucose'])
+st.table(positive_stats_table)
 
-st.write(df_src.query('diabetes == 1')['age'].mean())
-
-"Lets see the average bmi for those who don't have diabetes:"
-
-st.write(df_src.query('diabetes == 0')['bmi'].mean())
-
-"Lets see the average bmi for those who do have diabetes:"
-
-st.write(df_src.query('diabetes == 1')['bmi'].mean())
-
-"Lets see the average A1C for those who don't have diabetes:"
-
-st.write(df_src.query('diabetes== 0')['HbA1c_level'].mean())
-
-"Lets see the average a1c for those who do have diabetes:"
-
-st.write(df_src.query('diabetes == 1')['HbA1c_level'].mean())
-
-"Lets see the average blood glucose level for those who don't have diabetes:"
-
-st.write(df_src.query('diabetes== 0')['blood_glucose_level'].mean())
-
-"Lets see the average blood glucose level for those who do have diabetes:"
-
-st.write(df_src.query('diabetes == 1')['blood_glucose_level'].mean())
-
-"Lets see what percentage of those who have diabetes have heart disease:"
-
-st.write(len(df_src.query('diabetes == 1 and heart_disease==1'))/len(df_src.query('diabetes==1'))*100)
-
-"Lets see what percentage of those who have diabetes have hypertension:"
-
-st.write(len(df_src.query('diabetes == 1 and hypertension==1'))/len(df_src.query('diabetes==1'))*100)
+st.subheader('Percent with Comorbidities')
+percentage_stats_table = pd.DataFrame(data={'% With Heart Disease':[len(df_src.query('diabetes == 0 and heart_disease==1'))/len(df_src.query('diabetes==1'))*100,
+                                                             len(df_src.query('diabetes == 1 and heart_disease==1'))/len(df_src.query('diabetes==1'))*100],
+                                            '% With Hypertension':[len(df_src.query('diabetes == 0 and hypertension==1'))/len(df_src.query('diabetes==1'))*100,
+                                                             len(df_src.query('diabetes == 1 and hypertension==1'))/len(df_src.query('diabetes==1'))*100
+                                                  ]},index=['Diabetes Negative', 'Diabetes Positive'])
+st.table(percentage_stats_table)
--- pages/🔍Query Dataset.py
+++ pages/🔍Query Dataset.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.set_page_config(
     page_title="Query Dataset - WGU Capstone",
     page_icon="⚕️",
-    layout="wide",
+    layout="centered",
     initial_sidebar_state='auto'
 )
 
